113be4379445994c86db7f165ca9b3e9a4daf88c
lolobc <lauren.n.cameron@gmail.com>
2020-03-14 13:46:52 -0400

    push

--- script.js
+++ script.js
@@ -6,9 +6,16 @@ var opt1 = document.querySelector("#opt1");
 var opt2 = document.querySelector("#opt2");
 var opt3 = document.querySelector("#opt3");
 var opt4 = document.querySelector("#opt4");
+var addbtn = document.querySelector("#addbtn");
+var nameEl = document.querySelector("#name");
 var answerOpts = document.querySelectorAll(".optionButt")
 var wrongAnsPopup = document.querySelector("#wrongo");
+var highscores = document.querySelector("#highscores");
+
+
 var i = 0;
+// var people = [localStorage.getItem('name', 'score')];
+
 var theQuestions = [
     "What is Javascript?",
     "why is this so hard?",
@@ -71,6 +78,7 @@ function quiz () {
       opt3.textContent = optionsAtThree[0];
       opt4.textContent = optionsAtFour[0];
 
+    
   
 }
 
@@ -80,6 +88,41 @@ function quiz () {
 function nextQuestion () {
     event.preventDefault();
     i++;
+
+    if (i >= theQuestions.length) {
+        clearInterval(timeLeft);
+        document.getElementById('question').style.display = "none";
+        document.getElementById('answerOpts').style.display = "none";
+
+        document.getElementById('theHeader').style.display = "block";
+        theHeader.textContent = "You did it!";
+
+        document.getElementById('theContent').style.display = "block";
+        theContent.textContent = "Your final score is " + score; + "!";
+
+        document.getElementById('saveScoreForm').style.display = "block";
+
+        addbtn.addEventListener("click", function() {
+            event.preventDefault();
+            modal.style.display = "block";
+            var name = nameEl.value;
+            var li = document.createElement("li");
+            li.innerHTML = name + "  ------  " + score;
+            highscores.append(li);
+            var myScore = {
+                name: name,
+                score: score
+            };
+            // var myCoolScoreArray = [];
+            // myCoolScoreArray.push(myScore);
+            //[{name: "tony", score: 0}, {name:"lauren", score:100}]
+            localStorage.setItem("potato", JSON.stringify(myScore));
+            document.getElementById("addbtn").disabled = true;
+        });
+
+
+    }
+
     questionEl.textContent = theQuestions[i];
     opt1.textContent = optionsAtOne[i];
     opt2.textContent = optionsAtTwo[i];
@@ -93,6 +136,7 @@ function nextQuestion () {
 
 function wrongclick() { 
     document.getElementById('wrongo').style.display = "block";
+    score--;
     wrongAnsPopup.textContent = "Wrong! Try again!";
     setTimeout(function(){document.getElementById('wrongo').style.display = "none"; }, 1000);
 
@@ -152,6 +196,11 @@ var closeBtn = document.getElementsByClassName("close")[0];
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
+  var myScoreFromStorage = [localStorage.getItem("potato")];
+  myScoreFromStorage = JSON.parse(myScoreFromStorage);
+  var li = document.createElement("li");
+  li.innerHTML = myScoreFromStorage.name + "  wooooooo  " + myScoreFromStorage.score;
+  highscores.append(li);
 }
 
 // When the user clicks on <closeBtn> (x), close the modal
@@ -159,9 +208,3 @@ closeBtn.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
